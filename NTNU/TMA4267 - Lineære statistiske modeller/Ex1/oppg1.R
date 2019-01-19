519f423ea0024757d4fc61f994ccf14f3c34eb08
andersfagerli <anderstf@stud.ntnu.no>
2019-01-19 18:40:08 +0100

    Finished oppg1.R

--- NTNU/TMA4267 - Lineære statistiske modeller/Ex1/oppg1.R
+++ NTNU/TMA4267 - Lineære statistiske modeller/Ex1/oppg1.R
@@ -3,3 +3,14 @@ A
 B <- t(A)
 B
 P <- eigen(A)
+eigenval <- P$values
+eigenvec <- P$vectors
+eigen_matrix <- matrix(c(eigenval[1],0,0,eigenval[2]), nrow = 2, ncol = 2)
+diag_A <- eigenvec%*%eigen_matrix%*%t(eigenvec)
+corr_A <- cov2cor(A)
+
+E_x <- c(3,1)
+Cov_x = A
+
+C <- matrix(c(1,1,1,2), nrow = 2, ncol = 2)
+E_c = C%*%E_x
