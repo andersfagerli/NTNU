bc55ae9d09a6f1672b283ceaa5b51bbf4c0b63df
andersfagerli <anderstf@stud.ntnu.no>
2019-03-19 23:02:21 +0100

    Update assignment, shit doesnt work

--- NTNU/TDT4171 - Metoder i kunstig intelligens/Assignment 4/decisiontree.jl
+++ NTNU/TDT4171 - Metoder i kunstig intelligens/Assignment 4/decisiontree.jl
@@ -16,16 +16,18 @@ function DecisionTreeLearning(examples, attributes, parent_examples, set_random)
     else
         A = Importance(attributes,examples,set_random)
         tree = Node(A)
+        #println(examples)
+        #println(attributes)
+        #println(A)
         splice!(attributes, A) #Remove element at index A
         for v_k in [1 2]
             exs = Matrix(0,length(examples[1,:]))
             for i = 1:length(examples[:,end])
-                if (examples[i,end] == v_k)
+                if (examples[i,A] == v_k)
                     exs = [exs; examples[i,:]] #Appending a row to a matrix
                 end
             end
             subtree = DecisionTreeLearning(exs, attributes, examples, set_random)
-            println("Child: ", subtree.attribute, " Parent: ", tree.attribute)
             push!(tree.children, subtree)
         end
         return tree
@@ -38,8 +40,21 @@ function main()
     attributes = [1,2,3,4,5,6,7]
 
     tree = DecisionTreeLearning(training_examples, attributes, [], false)
-    randTree = DecisionTreeLearning(training_examples,attributes, [], true)
-
+    #randTree = DecisionTreeLearning(training_examples,attributes, [], true)
+    println("Root: ", tree.attribute)
+    println("Children: ", tree.children[1].attribute, tree.children[2].attribute)
+    tree = tree.children[2]
+    println("Root: ", tree.attribute)
+    println("Children: ", tree.children[1].attribute, tree.children[2].attribute)
+    tree = tree.children[1]
+    println("Root: ", tree.attribute)
+    println("Children: ", tree.children[1].attribute, tree.children[2].attribute)
+    tree = tree.children[1]
+    println("Root: ", tree.attribute)
+    println("Children: ", tree.children[1].attribute, tree.children[2].attribute)
+    tree = tree.children[1]
+    println("Root: ", tree.attribute)
+    println("Children: ", tree.children[1].attribute, tree.children[2].attribute)
 end
 
 main()
